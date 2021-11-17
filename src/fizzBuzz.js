805b0277a96667d375c60c00baded0d362f425fb
Jenaro Garcia <jjegarcia@hotmail.com>
2021-11-17 07:55:05 +0000

    fixed references to methods... so far no need for functions

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -8,8 +8,21 @@ class FizzBuzz {
     }
 
     fizzbuzz(number) {
-        return this.fizz(number) && this.buzz(number);
+        return number % 15 === 0;
     }
+
+    count(max) {
+        for (let number = 1; number <= max; number++) {
+            console.log(this.determine(number));
+        }
+    }
+    determine(number) {
+        if (this.fizzbuzz(number)){return "fizzbuzz"}
+        if (this.buzz(number)){ return "buzz"}
+        if (this.fizz(number)){ return "fizz"}
+        return number.toString()
+    }
+
 }
 
 module.exports = {
